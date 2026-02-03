422b301f0e7a543f83596f83ad0c24bc16168f1e
satyam mishra44 <mishrasatyam2060@gmail.com>
2026-02-03 18:32:22 +0530

    added userService inside the  service layer

--- src/main/java/com/example/Voucher/service/userService.java
+++ src/main/java/com/example/Voucher/service/userService.java
@@ -1,4 +1,19 @@
 package com.example.Voucher.service;
 
-public class userService {
+import com.example.Voucher.entity.User;
+
+import java.util.Optional;
+
+
+// This service acts as the business-layer abstraction
+// for all the user-related operations
+public interface userService {
+
+    //Fetch a user by their Unique ID
+    Optional<User> findById(Long userId);
+
+
+    boolean existsById(Long userId);
+
+    Optional<User> findByEmail(String email);
 }
